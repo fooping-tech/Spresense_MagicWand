8a2edf7876c2e3750d0fc4a13630c4c93df1245a
Fooping <fukuhala@gmail.com>
2024-01-21 17:25:05 +0900

    add M5ATOM

---
+++ src/M5ATOM_MagicWand_Parts/RINGLED.h
@@ -0,0 +1,32 @@
+#ifndef RINGLED_H
+#define RINGLED_H
+
+//#include <M5Unified.h>
+#include <M5Atom.h>
+
+#include <FastLED.h>
+//LED Setting
+#define FPS 120
+#define NUM_LEDS 1
+
+
+class RINGLED{
+  public:
+    RINGLED();
+    void setup(int);
+    void flash(int);//color
+    void clyon();
+    void fire();
+    void fire2(int,int);
+    void fade();
+    void inspection(int);
+    void stop();
+    void pacifica();
+    void setbrightness(int);//brightness
+    void counter(int);
+    void level(int);
+
+  private:
+    bool _isSetuped;
+};
+#endif
